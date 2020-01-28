0073349e60b6c0e68b3d3e57a014fdec168d4491
pkutaj <pkutaj@gmail.com>
2020-01-28 02:50:53 +0100

    Powershell > the input validation function

--- ui/powershell/curcon.ps1
+++ ui/powershell/curcon.ps1
@@ -7,9 +7,9 @@
 <# INTRO #>
 $intro = @"
   ____     
- / __ \  
-/ /  \ \ Mr. Paul's
-\ \__/ / Currency Converter
+ / __ \  Mr. Paul's
+/ /  \ \ Currency Converter
+\ \__/ / Quit with CTRL-C
  \____/  
 "@
 
@@ -26,16 +26,39 @@ function curCon {
         [Parameter(Mandatory = $true)]
         [string]
         $exchange_amount
-        
     )
-    $base_currency = $base_currency.toUpper();
-    $transaction_currency = $transaction_currency.toUpper();
-    if ($baseCurrency -eq "q") { exit }
-    node ..\..\app $base_currency $transaction_currency $exchange_amount
-    Pause
-    Clear-Host
+    $inputValidated = validateInput
+    if ($inputValidated -ne $true) { return } else {
+        if ($baseCurrency -eq "q") { exit }
+        node ..\..\app $base_currency $transaction_currency $exchange_amount
+        Pause
+        Clear-Host
+    }
 }
 
+function validateInput {
+    $inputError = "does not match any of the listed currencies, please try again and correct it"
+    $base_currency = $base_currency.toUpper();
+    $transaction_currency = $transaction_currency.toUpper();
+    [Bool]$inputValidated = $true
+    [Bool]$base_currency_Input_Validated = $false
+    [Bool]$transaction_currency_Input_Validated = $false
+    foreach ($currencyKey in $currencyKeys.Name) {
+        if ($base_currency -eq $currencyKey) { $base_currency_Input_Validated = $true }
+        if ($transaction_currency -eq $currencyKey) { $transaction_currency_Input_Validated = $true }
+    }
+    
+    if ($base_currency_Input_validated -ne $true) { 
+        write-host "The requested base currency $base_currency $inputError" -foregroundcolor Red
+        $inputValidated = $false
+    }
+    if ($transaction_currency_Input_validated -ne $true) { 
+        write-host "The requested transaction currency $transaction_currency $inputError" -foregroundcolor Red 
+        $inputValidated = $false
+    }
+    return $inputValidated
+    
+}
 
 write-host $intro
 Pause
@@ -45,7 +68,7 @@ Clear-Host
 $latestRates = Invoke-WebRequest -method GET https://api.exchangeratesapi.io/latest;
 $latestRatesParsed = $latestRates | ConvertFrom-Json;
 $output = $latestRatesParsed.rates | Out-String
-
+$currencyKeys = $latestRatesParsed.rates | Get-Member -type NoteProperty | Select-Object Name
 
 
 #S3 BIND THE INPUT
